c99962e7cc190affbb262b25f65b0ac4c3f56756
Jayaram <jayaramnaik725@gmail.com>
2022-07-08 22:46:36 +0530

    updated order_history

--- Routers/orderHistoryRouter.js
+++ Routers/orderHistoryRouter.js
@@ -5,8 +5,8 @@ const mysql = require('../database');
 router.get('/' , middleware , (req,res)=>{
     var sql = `select * from order_info where user_id = ${req.session.user.user_id}`;
     mysql.query(sql , (err, orders)=>{
-        console.log(orders);
-        
+        // console.log(orders);
+        orders.reverse();
         res.render('order_history' , {orders:orders});
     })
 })
