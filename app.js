1f09c8b966aa863110851f01425a3ce1b25edc4a
Jayaram <jayaramnaik725@gmail.com>
2022-07-08 22:34:21 +0530

    dep fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const middleware = require('./middleware')
 const bp = require('body-parser');
 const app = express();
 app.use(bp.urlencoded({extended:true}));
-const port = 3000;
+const port = process.env.PORT || 5000;
 app.use(express.static(__dirname+"/public"));
 app.set('view engine' , 'ejs');
 
