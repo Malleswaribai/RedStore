6cc8c8b5aafc5ca484aa3f73822a157381fcbebb
RAMAVATH JAYA RAM NAIK <76204860+ZeroOp@users.noreply.github.com>
2022-07-11 14:08:09 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 const conn = mysql.createConnection(
     {
-        host:"database-2.cjqpyslxmtms.us-west-2.rds.amazonaws.com",
+        host:"localhost",
         port:"3306",
         user:"admin",
         password:"zeroop123",
